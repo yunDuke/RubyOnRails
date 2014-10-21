141a07256281a5ab7d0346c2f3916d04ed7f9e90
YUN DU <duyunihan@gmail.com>
2014-10-21 15:57:24 +0100

    Finish sign in

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,8 +10,9 @@ class UsersController < ApplicationController
                                   :password, :password_confirmation)
         @user = User.new(secure_params)
         if @user.save
-        	flash[:success] = "Welcome to the Twitter App!" 
-            redirect_to @user# Handle a successful save.
+          sign_in @user       #  NEW LINE
+      flash[:success] = "Welcome to the Sample App!"    # NEW LINE
+      redirect_to @user
         else
             render 'new'# Handle an unsuccessful save.     
         end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,8 @@
  class User < ActiveRecord::Base
- 	 before_save { |user| user.email = email.downcase }
+ 	 before_save do |user| 
+              user.email = email.downcase 
+            user.remember_token = SecureRandom.urlsafe_base64
+            end
       validates :name, presence: true, length: { in: 9..30 }
       VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
       validates :email, presence: true, 
